a5f2be48f79565d861d0f71d57444e945faa6b22
skillerhero <skillerhero@users.noreply.github.com>
2019-11-01 10:31:41 -0600

    Update vistaLogueado.php

--- vista/vistaLogueado.php
+++ vista/vistaLogueado.php
@@ -11,7 +11,7 @@ include_once "../modelo/conexion.php";
 </head>
 <body>
 <div>
-<a href="agregarLibro.php">Crear libro</a>
+<a href="vista/agregarLibro.php">Crear libro</a>
 
 </div>
 <a href="../index.php">Cerrar Sesion</a>
@@ -50,12 +50,12 @@ foreach($result as $libro){
 
 
   
-<form action="../modelo/comentario.php">  
+<form action="../modelo/comentario.php" method="post">  
   <tr>
 
 
     <td><?php  echo $libro["titulo"]; ?>;</td>
-    <td><input name="nombre"></td>
+    <td><input name="nombre" value="<?php  echo $libro["autor"]?>"></td>
     <td><button type="submit">comentar</button></td>
 
   
