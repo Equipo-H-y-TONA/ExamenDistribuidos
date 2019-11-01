f3557de0854c1f6706c57ceb67108c36d6fa635e
miguelangelluis <52984840+miguelangelluis@users.noreply.github.com>
2019-11-01 10:17:40 -0600

    aaa

---
+++ vista/agregarLibro.php
@@ -0,0 +1,9 @@
+<form action="agregar.php" method="get">
+  <p>Titulo: <input type="text" name="titulo" size="40"></p>
+  <p>Autor: <input type="text" name="autor" size="40"></p>
+  
+  <p>
+    <input type="submit" value="Enviar">
+    <input type="reset" value="Borrar">
+  </p>
+</form>
