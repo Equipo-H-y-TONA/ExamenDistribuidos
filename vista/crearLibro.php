f69620f8f00d18306899f4ddba1bd09c8993fa99
Gabriel Rodriguez <48400046+bonnesbaby14@users.noreply.github.com>
2019-11-01 10:55:29 -0600

    kakak

--- vista/crearLibro.php
+++ vista/crearLibro.php
@@ -5,14 +5,10 @@ if(isset($_POST["titulo"]) && isset($_POST["autor"]) ){
       $titulo = $_POST["titulo"];
      $autor = $_POST["autor"];
     
-    $sql = $connect->query("insert into `libro`  values titulo= '" . $titulo . "' and autor='" . $autor . "'");
-     
-    $result = array();
+    $sql = $connect->query("INSERT INTO `libro` (`idLibro`, `titulo`, `autor`) VALUES(NULL, '".$titulo."', '".$autor."')");
     try{
     
-        while ($extraer = $sql->fetch_assoc()) {
-        $result[] = $extraer;
-    }
+     
     if(empty($result)){
 echo "usuario no existe";
 header("Location: ../index.php");
