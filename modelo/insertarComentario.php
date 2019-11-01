1a85a211ea3eff25e5a35353cde4879aeb5e8dd4
miguelangelluis <52984840+miguelangelluis@users.noreply.github.com>
2019-11-01 09:54:48 -0600

    insertarcomentario

---
+++ modelo/insertarComentario.php
@@ -0,0 +1,28 @@
+<?PHP
+include 'conexion.php';
+$json=array();
+/*Comprobar si se ha hecho un comentario antes*/
+    if(isset($_GET["fkUsuario"])&& isset($_POST["fkLibro"]) && isset($_POST["comentario"]) && isset($_POST["fecha"])){
+        $fkLibro=$_GET['fkLibro'];
+        $fkUsuario = $_GET['fkUsuario'];
+        $comentario= $_GET['comentario'];
+        $fecha = $_GET['fecha'];
+
+        $consulta="insert into comentario (comentario,fecha, fkUsuario, fkLibro) values ('".$comentario."','".$fecha."',".$fkUsuario.", ".$fkLibro.")";  
+        
+        if($resultado=mysqli_query($conexion, $consulta)){
+            $json['usuario'][] =array("exito"=>1,);
+        }else{
+          $resulta['usuario']=array("exito"=>-1,);
+          $json['usuario'][]=$resulta;
+        }
+
+        mysqli_close($conexion);
+        echo json_encode($json);
+    }
+    else{
+      $resulta["exito"]=0;
+      $json['usuario'][]=$resulta;
+      echo json_encode($json);
+    }
+?>
