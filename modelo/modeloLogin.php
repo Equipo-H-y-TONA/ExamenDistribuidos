8711a024543abecea9333c1fd8faf263345bc333
Gabriel Rodriguez <48400046+bonnesbaby14@users.noreply.github.com>
2019-11-01 09:04:07 -0600

    se cambio el select de password

--- modelo/modeloLogin.php
+++ modelo/modeloLogin.php
@@ -4,7 +4,7 @@ if(isset($_POST["mail"]) && isset($_POST["password"]) ){
       $mail = $_POST["mail"];
      $password = $_POST["password"];
     
-    $sql = $connect->query("SELECT usuario.*,desarrollador.* FROM desarrollador INNER JOIN usuario ON desarrollador.FK_USUARIO=usuario.ID_USUARIO where correo= '" . $mail . "' and password='" . $password . "'");
+    $sql = $connect->query("SELECT * FROM `usuario`  where nick= '" . $mail . "' and password='" . $password . "'");
      
     $result = array();
     try{
